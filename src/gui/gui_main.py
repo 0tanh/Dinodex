8e2a4aed0695dea2657469ba168972624d4eb8ea
Betty <betty.lossless@gmail.com>
2026-02-01 22:52:01 +0200

    giving up on real images for now. gonna freakign try ascii again

--- src/gui/gui_main.py
+++ src/gui/gui_main.py
@@ -1,40 +1,50 @@
+from tkinter import image_names
 import httpx
-
-from rich import panel
-import typer
 import textual
+import typer
+from rich import panel
 from textual import on
-from textual.widgets import Header, Footer,Static, Button
-from textual.containers import Grid, Itemgrid
 from textual.app import App, ComposeResult
+from textual.containers import Grid, ItemGrid
+from textual.widgets import Button, Footer, Header, Static
+from textual_image.widget.sixel import Image
+
+from rich.console import Console
 
 from ..cli.cli_main import collect
-from ..widget.Dino_Widgets import (MatrixNoise, Dino_Info, Dino_Pic)
+from ..widget.Dino_Widgets import Dino_Info, Dino_Pic, MatrixNoise
 
-class Dinodex(App): 
+class Dinodex(App):
+    
+    BINDING = [("c", "push_screen('DinoPic')")]
+    
+    console = Console()
     def compose(self):
         yield Header()
         with Grid():
             yield Button("collect!", name="Collect", id="CollectButton")
             yield Dino_Info(None, id="Dino_Info")
-            yield Dino_Pic(None, id="Dino_Pic")
+            # yield Dino_Pic(id="Dino_Pic")
         yield Footer()
+        
+    
     
     @on(Button.Pressed, "#CollectButton")
     async def collect_callback(self):
         async with httpx.AsyncClient():
             dino = await collect()
-        
+
         dino_info = self.query_one("#Dino_Info")
-        dino_info.dino_name = dino.name 
-        dino_info.dino_species = dino.species 
-        dino_info.dino_description = dino.description 
-        dino_info.dino_period = dino.period 
+        dino_info.dino_name = dino.name
+        dino_info.dino_species = dino.species
+        dino_info.dino_description = dino.description
+        dino_info.dino_period = dino.period
         dino_info.dino_movement = dino.movement
-        
+
         dino_pic = self.query_one("#Dino_Pic")
         dino_pic.dino_pic = dino.imageURL
 
+
 if __name__ == "__main__":
     app = Dinodex()
-    app.run() 
+    app.run()
--- src/widget/Dino_Widgets.py
+++ src/widget/Dino_Widgets.py
@@ -1,18 +1,16 @@
-import textual
-from textual.widgets import Static, TabbedContent
+from textual.widgets import Static 
 from textual.widget import Widget
 from textual.app import RenderResult, ComposeResult 
-from textual.strip import Strip, StripRenderable
-import textual.types
-from textual.reactive import Reactive, reactive
-from textual.containers import VerticalGroup, Grid, ItemGrid, Container
+from textual.strip import Strip 
+from textual.reactive import  reactive
+from textual.containers import  Container
 from rich.live import Live
-
+from textual.screen import Screen, ModalScreen
 
 from ..db.dino_classes import Dinosaur
 
+
 class Dino_Info(Static):
-    
     # dino_name = Reactive()
     dino_name = reactive("my name", recompose=True, repaint=True)
     dino_species = reactive("what dino", recompose=True)
@@ -20,7 +18,7 @@ class Dino_Info(Static):
     dino_period = reactive("from when", recompose=True)
     dino_movement = reactive("how i move", recompose=True)
     
-    def __init__(self, dino:Dinosaur|None, id):
+    def __init__(self, dino:Dinosaur|None, id=''):
         super().__init__()
         self.id = id
         self.dino = dino
@@ -36,18 +34,25 @@ class Dino_Info(Static):
             yield Static(self.dino_period)
             yield Static(self.dino_movement)        
 
-class Dino_Pic(Widget):
+class Dino_Pic(Screen):
     
     dino_pic = reactive("", recompose=True)
     
-    def __init__(self, dino:Dinosaur|None, id):
+    def __init__(self):
             super().__init__()
             self.id = id
             self.dino = dino
 
     def compose(self):
         yield Static(self.dino_pic)
+
+class Dino_Collect_With_Pic(Screen):
     
+    def compose(self):
+        yield Dino_Info(None)
+        yield 
+        
+
 class MatrixNoise(Widget):
     def render_line(self, y) -> Strip:
         
